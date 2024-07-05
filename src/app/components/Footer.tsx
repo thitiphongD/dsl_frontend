2801adb30f3ab9dc52d5df5507a720db5d6ac813
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-05 13:15:51 +0700

    first commit add navbar and footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="border-t fixed bottom-0 left-0 w-full p-2 text-center">
+      © 2024 DSL | Handmade in Bangkok
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const HorizontalNavbar: React.FC = () => {
+  return (
+    <nav className="flex-center gap-10 border-b p-2">
+      <div className="flex-center gap-2">
+        <Image
+          src="/favicon.ico"
+          width={40}
+          height={40}
+          alt="Picture of the author"
+        />
+        <span>DSL</span>
+      </div>
+
+      <div className="flex-center gap-4">
+        <Link href="/">Home</Link>
+        <Link href="/login">Login</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default HorizontalNavbar;
