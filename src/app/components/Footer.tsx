a12d3053dafc3b012dccaab2d6bfd452a4e1ef9f
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2024-07-05 17:03:34 +0700

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="border-t border-[#343434] fixed bottom-0 left-0 w-full p-2 text-center">
+    <div className="border-t border-[#343434] fixed bottom-0 left-0 w-full p-2 text-center text-[#7c7c7c]">
       © 2024 DSL | Handmade in Bangkok
     </div>
   );
